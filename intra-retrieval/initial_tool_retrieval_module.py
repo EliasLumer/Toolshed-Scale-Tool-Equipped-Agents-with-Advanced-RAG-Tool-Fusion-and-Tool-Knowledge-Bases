2bb8f8c76b3fb505083fe9fc8cb6090e6b6ad17d
EliasLumer <88982128+EliasLumer@users.noreply.github.com>
2024-10-23 17:48:51 -0400

    Update initial_tool_retrieval_module.py

--- intra-retrieval/initial_tool_retrieval_module.py
+++ intra-retrieval/initial_tool_retrieval_module.py
@@ -4,12 +4,11 @@ from intra_retrieval.base_artf_module import BaseARTFModules
 from pre_retrieval.vector_store_indexer import BaseVectorStoreIndexer
 
 class InitialToolRetrievalModule(BaseARTFModules):
-    def __init__(self, embedder: Optional[object], toolshed_knowledge_base: BaseVectorStoreIndexer):
-        super().__init__(embedder=embedder)
+    def __init__(self, toolshed_knowledge_base: BaseVectorStoreIndexer):
         self.toolshed_knowledge_base = toolshed_knowledge_base
 
     def generate(self, query: str, top_k: int) -> List[Document]:
-        """Queries the FAISS index using the provided query and returns the top_k results."""
+        """Queries the index using the provided query and returns the top_k results."""
         try:
             docs = self.toolshed_knowledge_base.query(query, k=top_k)
             return docs
@@ -17,7 +16,7 @@ class InitialToolRetrievalModule(BaseARTFModules):
             raise ValueError(f"Error retrieving tools: {str(e)}")
 
     async def agenerate(self, query: str, top_k: int) -> List[Document]:
-        """Asynchronous query to the FAISS index."""
+        """Asynchronous query to theindex."""
         try:
             docs = await self.toolshed_knowledge_base.aquery(query, k=top_k)
             return docs
