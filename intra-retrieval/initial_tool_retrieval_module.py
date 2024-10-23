05b8c2a7d37d6b94b6f311c441421a521f48f976
EliasLumer <88982128+EliasLumer@users.noreply.github.com>
2024-10-23 17:33:58 -0400

    Create initial_tool_retrieval_module.py

---
+++ intra-retrieval/initial_tool_retrieval_module.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from langchain_openai import ChatOpenAI,OpenAIEmbeddings
+from intra_retrieval.base_artf_module import BaseARTFModules
+from pre_retrieval.vector_store_indexer import BaseVectorStoreIndexer
+
+class InitialToolRetrievalModule(BaseARTFModules):
+    def __init__(self, embedder: Optional[object], toolshed_knowledge_base: BaseVectorStoreIndexer):
+        super().__init__(embedder=embedder)
+        self.toolshed_knowledge_base = toolshed_knowledge_base
+
+    def generate(self, query: str, top_k: int) -> List[Document]:
+        """Queries the FAISS index using the provided query and returns the top_k results."""
+        try:
+            docs = self.toolshed_knowledge_base.query(query, k=top_k)
+            return docs
+        except Exception as e:
+            raise ValueError(f"Error retrieving tools: {str(e)}")
+
+    async def agenerate(self, query: str, top_k: int) -> List[Document]:
+        """Asynchronous query to the FAISS index."""
+        try:
+            docs = await self.toolshed_knowledge_base.aquery(query, k=top_k)
+            return docs
+        except Exception as e:
+            raise ValueError(f"Error retrieving tools: {str(e)}")
